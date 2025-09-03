6db63edde991a642cd1a6893a1fc4b0e912d7757
mohammedismail1 <mohammedismail@talrop.com>
2025-09-04 00:17:10 +0530

    fix(service-card): fix

--- src/app/components/ui/ServicesCard.tsx
+++ src/app/components/ui/ServicesCard.tsx
@@ -12,9 +12,9 @@ const ServicesCard = ({ service }: any) => {
         height={50}
         className="mb-6"
       />
-      <h3 className="text-[24px] font-[500] mb-2">{service?.name}</h3>
+      <h3 className="text-[24px] font-[500] mb-2 leading-9">{service?.name}</h3>
       <p>{service?.description}</p>
-      <button className="absolute bottom-[-20px] left-1/2 px-[13.13px] py-[14.06px] bg-[#FFFFFF] rounded-[50%] shadow-[0px_4px_4px_rgba(0,_0,_0,_0.25)]">
+      <button className="absolute bottom-0 left-1/2 translate-x-[-50%] translate-y-1/2  px-[13.13px] py-[14.06px] bg-[#FFFFFF] rounded-[50%] shadow-[0px_4px_4px_rgba(0,_0,_0,_0.25)]">
         <Image
           src="assets/icons/right-arrow.svg"
           alt="right-arrow"
