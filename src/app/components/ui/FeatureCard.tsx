e9863c6388630dd95ee0ac321f13770744524007
mohammedismail1 <mohammedismail@talrop.com>
2025-09-03 23:58:36 +0530

    fix(FeatureCard): fix

--- src/app/components/ui/FeatureCard.tsx
+++ src/app/components/ui/FeatureCard.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const FeatureCard = ({ feature }: any) => {
   return (
-    <div className="bg-[#FFEFEA] p-6 rounded-[30px]  w-fit ">
+    <div className="bg-[#FFEFEA] p-6 rounded-[30px]  w-fit  cursor-pointer  hover:border-[#FE4A0F] hover:border-3">
       <h1 className="!text-[24px] !font-[500] mb-4">{feature.title}</h1>
       <p className="mb-6 !text-[16px] !text-[400]">{feature.description1}</p>
       <p className="!text-[16px] mb-12">{feature?.description2}</p>
