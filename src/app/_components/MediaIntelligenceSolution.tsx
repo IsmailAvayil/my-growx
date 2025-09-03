7a44fde427d11ab7301ee42c296b900800261743
mohammedismail1 <mohammedismail@talrop.com>
2025-09-04 01:09:56 +0530

    fix(media-int): fix

--- src/app/_components/MediaIntelligenceSolution.tsx
+++ src/app/_components/MediaIntelligenceSolution.tsx
@@ -24,17 +24,17 @@ const stats = [
 
 const MediaIntelligenceSolution = () => {
   return (
-    <div className="border border-[#FFB096] border-solid rounded-[30px] p-12 flex mt-[140px] mb-[120px] justify-between">
+    <div className="border border-[#FFB096] border-solid rounded-[30px] p-12 flex gap-[30px] mt-[140px] mb-[120px] justify-between">
       <div className="flex-1">
         <h1 className="mb-12">An award-winning media intelligence solution</h1>
-        <p>
+        <h2 className="!text-[24px] !font-[500] !leading-9">
           Confirmed as a high-performing product by <br /> customers and tech
           experts in various industries.
-        </p>
+        </h2>
       </div>
-      <div className="flex-1 grid grid-cols-2">
+      <div className="flex-1 grid grid-cols-2  gap-x-[39px] gap-y-[80px]">
         {stats.map((stat) => (
-          <div key={stat.id} className="stat-container">
+          <div key={stat.id}>
             <h1 className="text-[#FF6F40]">{stat.value}</h1>
             <p className="stat-description">{stat.description}</p>
           </div>
