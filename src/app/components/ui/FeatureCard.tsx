74ee4e725ddcda9ff088aa13b8e385939a5e18f9
mohammedismail1 <mohammedismail@talrop.com>
2025-09-03 23:53:52 +0530

    fix(featurecard): fix

--- src/app/components/ui/FeatureCard.tsx
+++ src/app/components/ui/FeatureCard.tsx
@@ -5,7 +5,7 @@ import React from "react";
 const FeatureCard = ({ feature }: any) => {
   return (
     <div className="bg-[#FFEFEA] p-6 rounded-[30px]  w-fit ">
-      <h1 className="!text-[24px] !font-[500] mb-6">{feature.title}</h1>
+      <h1 className="!text-[24px] !font-[500] mb-4">{feature.title}</h1>
       <p className="mb-6 !text-[16px] !text-[400]">{feature.description1}</p>
       <p className="!text-[16px] mb-12">{feature?.description2}</p>
       <Button text="GET STARTED" />
